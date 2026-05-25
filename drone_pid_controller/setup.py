94aa3f6addee6f0948aedb13c17be28ad733a21d
Ekansh <ekansh@mail.uni-paderborn.de>
2026-05-25 23:50:24 +0000

    Added PID controller for trajectory following

--- drone_pid_controller/setup.py
+++ drone_pid_controller/setup.py
@@ -11,7 +11,8 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/launch', ['launch/drone.launch.py']),
-        ('share/' + package_name + '/launch', ['launch/drone_snap.launch.py']),
+        ('share/' + package_name + '/launch', ['launch/drone_twist.launch.py']),
+        ('share/' + package_name + '/launch', ['launch/drone_twist_snap.launch.py']),
         ('share/' + package_name + '/config', ['config/drone_view.rviz']),
     ],
     install_requires=['setuptools'],
@@ -28,7 +29,8 @@ setup(
     entry_points={
         'console_scripts': [
             'controller_node = drone_pid_controller.drone_controller:main',
-            'snap_controller_node = drone_pid_controller.drone_controller_with_min_snap:main',
+            'twist_controller_node = drone_pid_controller.drone_twist_controller:main',
+            'snap_controller_node = drone_pid_controller.drone_twist_controller_with_min_snap:main',
         ],
     },
 )
---
+++ drone_simulation/launch/twist.sim.launch.py
@@ -0,0 +1,72 @@
+import os
+import shutil
+
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess, TimerAction, SetEnvironmentVariable
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+
+    pkg_name = 'drone_simulation'
+    pkg_share = get_package_share_directory(pkg_name)
+
+    models_dir = os.path.join(pkg_share, 'models')
+
+    # ---------------------------------------------------
+    # Detect simulator
+    # ---------------------------------------------------
+    use_gz = shutil.which('gz') is not None
+    use_ign = shutil.which('ign') is not None
+
+    if use_gz:
+        gazebo_cmd = ['gz', 'sim', '-v', '4', os.path.join(pkg_share, 'worlds', 'drone_world_twist.sdf')]
+
+        resource_env = SetEnvironmentVariable(
+            name='GZ_SIM_RESOURCE_PATH',
+            value=models_dir
+        )
+
+        msg_prefix = 'gz.msgs'
+
+    elif use_ign:        
+        gazebo_cmd = ['ign', 'gazebo', '-v', '4', os.path.join(pkg_share, 'worlds', 'drone_world_ign_twist.sdf')]
+
+        resource_env = SetEnvironmentVariable(
+            name='IGN_GAZEBO_RESOURCE_PATH',
+            value=models_dir
+        )
+
+        msg_prefix = 'ignition.msgs'
+
+    else:
+        raise RuntimeError("Neither 'gz' nor 'ign' Gazebo found")
+
+    gazebo = ExecuteProcess(
+        cmd=gazebo_cmd,
+        output='screen'
+    )
+
+    ros_bridge = TimerAction(
+        period=3.0,
+        actions=[
+            Node(
+                package='ros_gz_bridge',
+                executable='parameter_bridge',
+                name='sim_bridge',
+                output='screen',
+                arguments=[
+                    f'/X3/gazebo/command/motor_speed@actuator_msgs/msg/Actuators@{msg_prefix}.Actuators',
+                    f'/X3/gazebo/command/twist@geometry_msgs/msg/Twist@{msg_prefix}.Twist',
+                    f'/world/quadcopter/pose/info@geometry_msgs/msg/PoseArray@{msg_prefix}.Pose_V',
+                ]
+            )
+        ]
+    )
+
+    return LaunchDescription([
+        resource_env,
+        gazebo,
+        ros_bridge,
+    ])
